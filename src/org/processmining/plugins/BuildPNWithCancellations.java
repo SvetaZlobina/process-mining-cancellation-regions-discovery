7f3a3687d01841154eb340a6fd011b518c50ef92
zlobinasv <zlobinasv@yandex-team.ru>
2021-05-08 17:28:11 +0300

    feat(*): Add experiment results for motivating example

--- src/org/processmining/plugins/BuildPNWithCancellations.java
+++ src/org/processmining/plugins/BuildPNWithCancellations.java
@@ -9,16 +9,14 @@ import org.processmining.models.connections.transitionsystem.MinimalRegionConnec
 import org.processmining.models.connections.transitionsystem.RegionConnection;
 import org.processmining.models.graphbased.directed.petrinet.Petrinet;
 import org.processmining.models.graphbased.directed.petrinet.ResetNet;
-import org.processmining.models.graphbased.directed.petrinet.impl.PetrinetImpl;
 import org.processmining.models.graphbased.directed.petrinet.impl.ResetNetImpl;
 import org.processmining.models.graphbased.directed.transitionsystem.State;
 import org.processmining.models.graphbased.directed.transitionsystem.Transition;
 import org.processmining.models.graphbased.directed.transitionsystem.TransitionSystem;
 import org.processmining.models.graphbased.directed.transitionsystem.TransitionSystemImpl;
+import org.processmining.models.graphbased.directed.transitionsystem.regions.Region;
 import org.processmining.models.graphbased.directed.transitionsystem.regions.RegionSet;
 import org.processmining.plugins.ts.ui.TSDecomposeUI;
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -36,23 +34,37 @@ public class BuildPNWithCancellations {
     public ResetNet constructTS(UIPluginContext context, TransitionSystem initialTs) throws ConnectionCannotBeObtained {
         List<Object> cancellationStates = new TSDecomposeUI(context).decompose(initialTs);
 
+        //step 0 - Prepare TS: merge the states with identical outflow
+
         // step 1 - Construct a TS' from TS by deleting the cancellation state and its incident arcs
         TransitionSystem tsWithoutCancellations = excludeCancellationStatesFromTs(initialTs, cancellationStates);
 
-        // step 2 -
+        // step 2 - Verify that cancellation sets are minimal regions
         Map<Object, List<Transition>> failureEventsTransitionsByEventName =
                 getFailureEvents(initialTs, cancellationStates);
         List<Set<State>> cancellationSets = getCancellationSets(failureEventsTransitionsByEventName);
-        // TODO: verify, that all cancellation sets are minimal regions
+
         RegionSet regions = context.tryToFindOrConstructFirstObject(RegionSet.class, MinimalRegionConnection.class,
                 RegionConnection.REGIONS, tsWithoutCancellations);
 
+        // TODO: is it the right way of verification?
+        for (Region minimalRegion : regions) {
+            for (Set<State> cancellationSet : cancellationSets) {
+                if (cancellationSet.containsAll(minimalRegion) && cancellationSet.size() > minimalRegion.size()) {
+                    throw new RuntimeException(
+                            "RWF-net cannot be constructed, because cancellation set is not a minimal region");
+                }
+            }
+        }
+
         // step 3 - Construct WF' from TS' using state-based region algorithm
-        Petrinet petriNet2 = context.tryToFindOrConstructFirstObject(Petrinet.class, null, null,
+        Petrinet petriNet = context.tryToFindOrConstructFirstObject(Petrinet.class, null, null,
                 tsWithoutCancellations);
 
+        // step 4 - Add to WF' transitions corresponding to failure and catching events
+        // TODO
+
         return new ResetNetImpl(initialTs.getLabel());
-//        return petriNet2;
     }
 
     private TransitionSystem excludeCancellationStatesFromTs(TransitionSystem transitionSystem,
